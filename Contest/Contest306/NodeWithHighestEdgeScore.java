d27470d7e034799f3afede65683da340ebf0e20d
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2022-08-14 09:36:56 +0200

    Question2

---
+++ Contest/Contest306/NodeWithHighestEdgeScore.java
@@ -0,0 +1,25 @@
+package Contest.Contest306;
+
+public class NodeWithHighestEdgeScore {
+    public int edgeScore(int[] edges){
+        int length = edges.length;
+        int[] labels = new int[length];
+        for(int i = 0; i < length; i++){
+            labels[edges[i]] += i;
+            if(labels[edges[i]] < 0){
+                labels[edges[i]] = Integer.MAX_VALUE;
+            }
+        }
+        int max = labels[0];
+        int maxNode = 0;
+        for(int i = 1; i < length; i++){
+            if(max < labels[i]){
+                maxNode = i;
+                max = labels[i];
+            }else if((max == labels[i]) && (maxNode > i)){
+                maxNode = i;
+            }
+        }
+        return maxNode;
+    }
+}
