45b6a49fe4dd791094bf28f996adbf95b235ef56
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2023-02-26 10:23:26 +0100

    Best time to buy and sell stock II solution

---
+++ interview_questions/array/Best_time_to_buy_and_sell_stock_2.java
@@ -0,0 +1,13 @@
+package interview_questions.array;
+
+class Best_time_to_buy_and_sell_stock_2{
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++){
+            if(prices[i] > prices[i - 1]){
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
+    }
+}
--- interview_questions/array/Remove_duplicates_from_sorted_array.java
+++ interview_questions/array/Remove_duplicates_from_sorted_array.java
@@ -1,4 +1,4 @@
-package interview_questions.strings;
+package interview_questions.array;
 
 public class Remove_duplicates_from_sorted_array {
     public int removeDuplicates(int[] nums) {
