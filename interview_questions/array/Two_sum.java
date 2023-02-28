b02d257024392f0a3b2174de54015cfa19e930b6
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2023-02-28 18:15:31 +0100

    fix package issue

---
+++ interview_questions/array/Two_sum.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class Two_sum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        HashMap<Integer, Integer> container = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++){
+            int difference = target - nums[i];
+            if(container.containsKey(difference)){
+                res[0] = container.get(difference);
+                res[1] = i;
+                return res;
+            } else {
+                container.put(nums[i], i);
+            }
+        }
+        return res;
+    }
+}
