a3cdec76efa7cf8815a28d6da3910949622c94ba
Tuan Hoang <Orca@Tuans-MacBook-Pro.local>
2023-02-28 08:04:40 +0100

    Move Zeroes solution

---
+++ interview_questions/array/Contains_duplicate.java
@@ -0,0 +1,13 @@
+
+
+class Contains_duplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> container = new HashSet<Integer>();
+        for (int element : nums){
+            if (!container.add(element)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ interview_questions/array/Plus_one.java
@@ -0,0 +1,15 @@
+public class Plus_one {
+    public int[] plusOne(int[] digits) {
+        int length = digits.length;        
+        for (int i = length - 1; i >= 0; i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] res = new int[length + 1];
+        res[0] = 1;
+        return res;
+    }
+}
