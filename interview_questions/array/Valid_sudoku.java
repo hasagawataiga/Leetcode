5de912db9250580d99a286c57c589e4f5ab3df7d
Tuan Hoang <Orca@Tuans-MacBook-Pro.local>
2023-03-01 00:40:38 +0100

    Rotate Image solution

--- interview_questions/array/Valid_sudoku.java
+++ interview_questions/array/Valid_sudoku.java
@@ -2,21 +2,20 @@ public class Valid_sudoku {
     public boolean isValidSudoku(char[][] board) {
         int[] rows = new int[9];
         int[] cols = new int[9];
-        int[] boxes = new int[9];
-    
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
+        int[] grids = new int[9];
+        for (int i = 0; i < 9; i++){
+            for (int j = 0; j < 9; j++){
                 if (board[i][j] == '.') {
                     continue;
                 }
-                int element = Character.getNumericValue(board[i][j]);
-                int mask = 1 << (element - 1);
-                if ((rows[i] & mask) != 0 || (cols[j] & mask) != 0 || (boxes[(i / 3) * 3 + j / 3] & mask) != 0) {
+                int value = Character.getNumericValue(board[i][j]);
+                int mask = 1 << (value - 1);
+                if ((rows[i] & mask) != 0 || (cols[j] & mask) != 0 || (grids[i/3*3 + j/3] & mask) != 0){
                     return false;
                 }
                 rows[i] |= mask;
                 cols[j] |= mask;
-                boxes[(i / 3) * 3 + j / 3] |= mask;
+                grids[i/3*3 +j/3] |= mask;
             }
         }
         return true;
