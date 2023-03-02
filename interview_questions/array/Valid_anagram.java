323b72a05df404ffe33ab658a366873bcdbf1f1d
hasagawataiga <hoangvoletuan1603@gmail.com>
2023-03-02 17:42:25 +0100

    Optimized Valid Anagram solution

---
+++ interview_questions/array/Valid_anagram.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public class Valid_anagram {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()){
+            return false;   
+        }
+        int[] container = new int[26];
+        char[] sCharArray = s.toCharArray();
+        char[] tCharArray = t.toCharArray();
+        for (int i = 0; i < sCharArray.length; i++){
+            container[sCharArray[i] - 'a']++;
+            container[tCharArray[i] - 'a']--;
+        }
+        for (int i : container){
+            if (i != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
