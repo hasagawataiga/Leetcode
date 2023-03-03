2585175979647dc5fbbf9f4a6eefdcfdcadfd7f8
hasagawataiga <hoangvoletuan1603@gmail.com>
2023-03-03 18:35:47 +0100

    optimized Longest Common Prefix solution from 1ms to 0ms and add more comments

--- interview_questions/array/Longest_common_prefix.java
+++ interview_questions/array/Longest_common_prefix.java
@@ -3,21 +3,17 @@ public class Longest_common_prefix {
         int shortestStrIndex = 0;
         // Find the most shortest String in array
         for (int i = 0; i < strs.length; i++){
-            if (strs[i].equals("")){
-                return "";
-            }
             if (strs[shortestStrIndex].length() > strs[i].length()){
                 shortestStrIndex = i;
             }
         }
-        if (strs.length < 2){
-            return strs[0];
-        }
         // Compares every char in the shortest string to the other strings in the array
         // the index of chars are similar in every single strings
         for (int i = 0; i < strs[shortestStrIndex].length(); i++){
+            // Use a char variable to reduce the times .charAt() method will be called
+            char ch = strs[shortestStrIndex].charAt(i);
             for (int j = 0; j < strs.length; j++){
-                if (strs[shortestStrIndex].charAt(i) != strs[j].charAt(i)){
+                if (ch != strs[j].charAt(i)){
                     return strs[shortestStrIndex].substring(0, i);
                 }
             }
