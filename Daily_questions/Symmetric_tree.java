f3940f1fe6b43538697f97bcc692d9682e200b25
hasagawataiga <hoangvoletuan1603@gmail.com>
2023-03-13 11:35:54 +0100

    Symmetric Tree solution

---
+++ Daily_questions/Symmetric_tree.java
@@ -0,0 +1,21 @@
+package Daily_questions;
+
+import interview_questions.trees.TreeNode;
+
+public class Symmetric_tree {
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmetricTree(root.left, root.right);
+    }
+    private boolean isSymmetricTree(TreeNode left, TreeNode right){
+        if (left == null && right == null){
+            return true;
+        }
+        if (left == null || right == null){
+            return false;
+        }
+        if (left.val != right.val){
+            return false;
+        }
+        return isSymmetricTree(left.left, right.right) && isSymmetricTree(left.right, right.left);
+    }
+}
