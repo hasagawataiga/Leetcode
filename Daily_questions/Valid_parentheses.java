6fd25b48d52055f8d5539ace2f0662908c7e8bff
hasagawataiga <hoangvoletuan1603@gmail.com>
2023-03-13 13:39:54 +0100

    optimized Valid parentheses solution

--- Daily_questions/Valid_parentheses.java
+++ Daily_questions/Valid_parentheses.java
@@ -1,43 +1,40 @@
 package Daily_questions;
 
-import java.util.Stack;
-
 public class Valid_parentheses {
     public boolean isValid(String s) {
-        if (s.isEmpty()){
-            return true;
-        }
         int length = s.length();
-        // The string must has even length
+		// The string must has even length
         if (length%2 == 1){
             return false;
         }
-        char[] array = new char[length];
-        for (int i = 0; i < length; i++){
-            array[i] = s.charAt(i);
-        }
-        // Use stack to store the order and the number of parentheses
-        //  traversal the string 
-        //  whenever reachs a close parentheses, we check whether it is proper position and character
-        Stack<Character> stack = new Stack<>();
-        stack.push(array[0]);
-        int i = 1;
-        while (i < length){
-            if (array[i] == '(' || array[i] == '[' || array[i] == '{'){
-                stack.push(array[i]);
-            } else if (stack.empty()){
+		// char[] as a stack
+		// push the open parentheses 
+		// and check whether the close parentheses is proper position and character whenever reachs it
+        char[] stack = new char[length];
+        // We want to check the s.charAt(i) to the last open parentheses in stack
+        //  so that why we use stack[++pointer] for store open parentheses and stack[pointer] (pointer-- after that)
+        //  instead of init pointer = 0, stack[pointer] (pointer++ after that) for store open parentheses
+        //      this make the code more complex
+        int pointer = -1;
+        int index = 0;
+        while (index < length){
+            char ch = s.charAt(index++);
+            if (ch == '(' || ch == '[' || ch == '{'){
+                stack[++pointer] = ch;
+            } else if (pointer < 0){
                 return false;
-            } else if ((array[i] == ')') && (stack.peek() == '(')){
-                stack.pop();
-            } else if ((array[i] == ']') && (stack.peek() == '[')){
-                stack.pop();
-            } else if ((array[i] == '}') && (stack.peek() == '{')){
-                stack.pop();
+            } else if ((ch == ')') && (stack[pointer] == '(')){
+                pointer--;
+            } else if ((ch == ']') && (stack[pointer] == '[')){
+                pointer--;
+            } else if ((ch == '}') && (stack[pointer] == '{')){
+                pointer--;
             } else {
                 return false;
             }
-            i++;
         }
-        return stack.empty() && (i == length);
+        // If the parentheses are valid, the pointer at the end must be -1
+        //  otherwise, there would be at least 1 character left in the stack
+        return pointer == -1;
     }
 }
