0040b5aef9478bc23985cb73136f10c3f1ac7571
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2023-03-16 10:28:43 +0100

    Swap Nodes in Pairs solution

---
+++ Recursion/Principle_of_recursion/Swap_nodes_in_pairs.java
@@ -0,0 +1,20 @@
+package Recursion.Principle_of_recursion;
+
+import interview_questions.linked_list.ListNode;
+
+public class Swap_nodes_in_pairs {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null){
+            return head;
+        }
+        ListNode nextNode = head.next;
+        if (nextNode == null){
+            head.next = null;
+            return head;
+        } else {
+            head.next = swapPairs(head.next.next);
+            nextNode.next = head;   
+        }
+        return nextNode;
+    }
+}
