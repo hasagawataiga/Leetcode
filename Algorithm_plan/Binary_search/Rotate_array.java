bec3ec5652055a12d256425ea3b3821572fb27a2
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2023-03-20 19:55:23 +0100

    Rotate Array solution

---
+++ Algorithm_plan/Binary_search/Rotate_array.java
@@ -0,0 +1,32 @@
+package Algorithm_plan.Binary_search;
+
+// Reverse the whole array
+// Reverse the first k elements
+// Reverse the entire elements (k -> length - 1)
+public class Rotate_array {
+    public void rotate(int[] nums, int k) {
+        int length = nums.length;
+        k = k % length;
+        if (k == 0){
+            return;
+        }
+        if (length == 1){
+            return;
+        }
+        reverse(nums, 0, length - 1);
+        reverse(nums, k, length - 1);
+        reverse(nums, 0, k - 1);
+    }
+    private void swap(int[] nums, int a, int b){
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+    private void reverse(int[] nums, int left, int right){
+        while (left < right){
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+}
