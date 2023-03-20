647b998abd41def0d9f3dd4dc35f44d2c381f1c5
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2023-03-20 19:19:42 +0100

    Optimized Squares of a Sorted Array solution

--- Algorithm_plan/Binary_search/Squares_of_a_sorted_array.java
+++ Algorithm_plan/Binary_search/Squares_of_a_sorted_array.java
@@ -1,11 +1,28 @@
 package Algorithm_plan.Binary_search;
 
+import java.util.Arrays;
+
+// Use 2 pointers
+// Compare the squares of left and right
+// Which one greater would be placed at the res index (start from the end of array)
 public class Squares_of_a_sorted_array {
     public int[] sortedSquares(int[] nums) {
-        for (int i = 0; i < nums.length; i++){
-            nums[i] *= nums[i];
+        int[] res = nums.clone();
+        int left = 0;
+        int right = nums.length - 1;
+        int i = right;
+        while (left <= right){
+            int leftVal = nums[left] * nums[left];
+            int rightVal = nums[right] * nums[right];
+            if (leftVal > rightVal){
+                res[i] = leftVal;
+                left++;
+            } else {
+                res[i] = rightVal;
+                right--;
+            }
+            i--;
         }
-        Arrays.sort(nums);
-        return nums;
+        return res;
     }
 }
