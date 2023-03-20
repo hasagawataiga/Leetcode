064ef97577dc9677ecc56f802362c5137f333c68
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2023-03-20 17:48:23 +0100

    Two Sum solution

---
+++ Data_structure_plan/Data_structure_I/Two_sum.java
@@ -0,0 +1,22 @@
+package Data_structure_plan.Data_structure_I;
+
+import java.util.HashMap;
+
+public class Two_sum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        HashMap<Integer, Integer> container = new HashMap<>();
+        int length = nums.length;
+        for (int i = 0; i < length; i++){
+            int num = nums[i];
+            if (container.containsKey(num)){
+                res[0] = container.get(num);
+                res[1] = i;
+                return res;
+            } else {
+                container.put(target - num, i);
+            }
+        }
+        return res;
+    }
+}
--- Dynamic_programming_plan/Dynamic_programming_I/Min_cost_climbing_stairs.java
+++ Dynamic_programming_plan/Dynamic_programming_I/Min_cost_climbing_stairs.java
@@ -2,12 +2,13 @@ package Dynamic_programming_plan.Dynamic_programming_I;
 
 public class Min_cost_climbing_stairs {
     public int minCostClimbingStairs(int[] cost) {
-        int[] mins = new int[cost.length + 1];
-        mins[0] = 0;
-        mins[1] = cost[0];
-        for (int i = 2; i < mins.length; i++){
-            mins[i] = Math.min(mins[i - 1], mins[i - 2]) + cost[i - 1];
+        int length = cost.length;
+        int[] mins = new int[length];
+        mins[0] = cost[0];
+        mins[1] = cost[1];
+        for (int i = 2; i < length; i++){
+            mins[i] = Math.min(mins[i - 1], mins[i - 2]) + cost[i];
         }
-        return Math.min(mins[mins.length - 1], mins[mins.length - 2]);
+        return Math.min(mins[length - 1], mins[length - 2]);
     }
 }
