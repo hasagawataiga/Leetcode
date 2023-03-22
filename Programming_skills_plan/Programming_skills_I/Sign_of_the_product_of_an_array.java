b919fc3d37200d2e13d4ce095d5bc988e7aa1257
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2023-03-22 19:09:17 +0100

    Sign of the Product of an Array solution

---
+++ Programming_skills_plan/Programming_skills_I/Sign_of_the_product_of_an_array.java
@@ -0,0 +1,18 @@
+package Programming_skills_plan.Programming_skills_I;
+
+public class Sign_of_the_product_of_an_array {
+    public int arraySign(int[] nums) {
+        int negative = 0;
+        for (int num : nums){
+            if (num == 0){
+                return 0;
+            } else if (num < 0){
+                negative++;
+            }
+        }
+        if (negative > 0 && negative%2 == 1){
+            return -1;
+        }
+        return 1;
+    }
+}
