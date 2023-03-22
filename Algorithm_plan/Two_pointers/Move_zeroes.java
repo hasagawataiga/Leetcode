e3c8695c1f3f1dd3b92fd669d20e1a3ad3e4b9d0
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2023-03-22 22:15:15 +0100

    Reverse String solution

--- Algorithm_plan/Two_pointers/Move_zeroes.java
+++ Algorithm_plan/Two_pointers/Move_zeroes.java
@@ -1,32 +1,17 @@
 package Algorithm_plan.Two_pointers;
 
 public class Move_zeroes {
-    public int[] twoSum(int[] numbers, int target) {
-        int[] res = new int[2];
-        int left = 0;
-        int right = numbers.length - 1;
-        while (left < right){
-            int middle = left + (right - left) / 2;
-            int leftVal = numbers[left];
-            int rightVal = numbers[right];
-            int sum = leftVal + rightVal;
-            int middleVal = numbers[middle];
-            if (sum < target){
-                if (middleVal + rightVal < target){
-                    left = middle;
-                }
-                left++;
-            } else if (sum > target){
-                if (leftVal + middleVal > target){
-                    right = middle;
-                }
-                right--;
-            } else {
-                res[0] = left + 1;
-                res[1] = right + 1;
-                return res;
+    public void moveZeroes(int[] nums) {
+        int pointer = 0;
+        int length = nums.length;
+        for (int i = 0; i < length; i++){
+            if (nums[i] != 0){
+                nums[pointer] = nums[i];
+                pointer++;
             }
         }
-        return res;
+        for (int i = pointer; i < length; i++){
+            nums[i] = 0;
+        }
     }
 }
---
+++ Algorithm_plan/Two_pointers/Reverse_string.java
@@ -0,0 +1,15 @@
+package Algorithm_plan.Two_pointers;
+
+public class Reverse_string {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
