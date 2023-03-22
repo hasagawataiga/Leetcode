0a156d3742cf1548a9d41dba1792562ddcf504fe
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2023-03-22 23:07:19 +0100

    Best Time to Buy and Sell Stock solution

---
+++ Leetcode_75/Greedy/Best_time_to_buy_and_sell_stock.java
@@ -0,0 +1,18 @@
+package Leetcode_75.Greedy;
+
+public class Best_time_to_buy_and_sell_stock {
+    public int maxProfit(int[] prices) {
+        if (prices.length < 2){
+            return 0;
+        }
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for (int price : prices){
+            if (price < minPrice){
+                minPrice = price;
+            }
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+        return maxProfit;
+    }
+}
