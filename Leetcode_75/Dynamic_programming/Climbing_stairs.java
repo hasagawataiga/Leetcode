c54f3b4053d48a1e3856c52e8da019f94fac9fdf
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2023-03-28 00:18:24 +0200

    Climbing Stairs solution

---
+++ Leetcode_75/Dynamic_programming/Climbing_stairs.java
@@ -0,0 +1,22 @@
+package Leetcode_75.Dynamic_programming;
+
+public class Climbing_stairs {
+    int[] maxs = new int[46];
+    public int climbStairs(int n) {
+        // Base case
+        if (n == 1 || n == 2){
+            maxs[n] = n;
+            return maxs[n];
+        }
+        // Recursion
+        if (maxs[n - 1] == 0){
+            maxs[n - 1] = climbStairs(n - 1);
+        }
+        if (maxs[n - 2] == 0){
+            maxs[n - 2] = climbStairs(n - 2);
+        }
+        maxs[n] = maxs[n - 1] + maxs[n - 2];
+        return maxs[n];
+    }
+}
+
