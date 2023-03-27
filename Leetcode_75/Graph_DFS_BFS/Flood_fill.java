1bf7ea5299cb476513f0fd74b8f4550571d44374
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2023-03-27 18:11:06 +0200

    Optimized Flood Fill solution

--- Leetcode_75/Graph_DFS_BFS/Flood_fill.java
+++ Leetcode_75/Graph_DFS_BFS/Flood_fill.java
@@ -2,48 +2,27 @@ package Leetcode_75.Graph_DFS_BFS;
 
 public class Flood_fill {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        boolean[][] visited = new boolean[image.length][image[0].length];
-        floodFill(image, sr, sc, color, visited);
+        int startingColor = image[sr][sc];
+        if (startingColor == color){
+            return image;
+        }
+        floodFill(image, sr, sc, color, startingColor);
         return image;
     }
-    private void floodFill(int[][] image, int sr, int sc, int color, boolean[][] visited){
-        if (visited[sr][sc]){
+    private void floodFill(int[][] image, int sr, int sc, int color, int startingColor){
+        if (sr < 0 || sr >= image.length){
             return;
-        } else {
-            visited[sr][sc] = true;
         }
-        if (sr > 0 && sr < image.length - 1){
-            if (image[sr - 1][sc] == image[sr][sc]){
-                floodFill(image, sr - 1, sc, color, visited);
-            }
-            if (image[sr + 1][sc] == image[sr][sc]){
-                floodFill(image, sr + 1, sc, color, visited);
-            }
-        } else if (sr > 0){
-            if (image[sr - 1][sc] == image[sr][sc]){
-                floodFill(image, sr - 1, sc, color, visited);
-            }
-        } else {
-            if (image[sr + 1][sc] == image[sr][sc]){
-                floodFill(image, sr + 1, sc, color, visited);
-            }
+        if (sc < 0 || sc >= image[sr].length){
+            return;
         }
-        if (sc > 0 && sc < image[sr].length - 1){
-            if (image[sr][sc - 1] == image[sr][sc]){
-                floodFill(image, sr, sc - 1, color, visited);
-            }
-            if (image[sr][sc + 1] == image[sr][sc]){
-                floodFill(image, sr, sc + 1, color, visited);
-            }
-        } else if (sc > 0){
-            if (image[sr][sc - 1] == image[sr][sc]){
-                floodFill(image, sr, sc - 1, color, visited);
-            }
-        } else {
-            if (image[sr][sc + 1] == image[sr][sc]){
-                floodFill(image, sr, sc + 1, color, visited);
-            }
+        if (image[sr][sc] != startingColor){
+            return;
         }
         image[sr][sc] = color;
+        floodFill(image, sr - 1, sc, color, startingColor);
+        floodFill(image, sr + 1, sc, color, startingColor);
+        floodFill(image, sr, sc - 1, color, startingColor);
+        floodFill(image, sr, sc + 1, color, startingColor);
     }
 }
