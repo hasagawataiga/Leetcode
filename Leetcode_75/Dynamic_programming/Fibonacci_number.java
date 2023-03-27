6f2aab7d1fb9adb8aaf2cdbb4ca7979f27ac64d8
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2023-03-28 00:17:33 +0200

    Fibonacci Number solution

---
+++ Leetcode_75/Dynamic_programming/Fibonacci_number.java
@@ -0,0 +1,18 @@
+package Leetcode_75.Dynamic_programming;
+public class Fibonacci_number{
+    int[] nums = new int[30];
+    public int fib(int n) {
+        if (n == 1 || n == 0){
+            nums[n] = n;
+            return nums[n];
+        }
+        if (nums[n - 1] == 0){
+            nums[n - 1] = fib(n - 1);
+        }
+        if (nums[n - 2] == 0){
+            nums[n - 2] = fib(n - 2);
+        }
+        
+        return nums[n - 1] + nums[n - 2];
+    }
+}
