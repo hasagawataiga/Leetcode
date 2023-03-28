3e334340c0d824e84190523697dacd36e0009a48
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2023-03-28 22:37:03 +0200

    Unique paths solution

--- Leetcode_75/Dynamic_programming/Unique_paths.java
+++ Leetcode_75/Dynamic_programming/Unique_paths.java
@@ -3,19 +3,18 @@ package Leetcode_75.Dynamic_programming;
 public class Unique_paths {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        dp[0][0] = 0;
-    }
-    private void moves(int[][] dp, int m, int n){
-        if ((m == dp.length - 1) && (n == dp[m].length - 1)){
-            return;
+        dp[0][0] = 1;
+        for (int i = 1; i < m; i++){
+            dp[i][0] = 1;
         }
-        if (m == dp.length - 1){
-            dp[m][n] = dp[m][n - 1];
-            return;
+        for (int i = 1; i < n; i++){
+            dp[0][i] = 1;
         }
-        if (n == 0){
-            dp[m][n] = dp[m - 1][n];
+        for (int i = 1; i < m; i++){
+            for (int j = 1; j < n; j++){
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
         }
-
+        return dp[m - 1][n - 1];
     }
 }
