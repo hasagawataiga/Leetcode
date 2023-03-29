fcbcfd4580ac65871fecd6bbaf42e03bcb7227f6
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2023-03-29 11:42:13 +0200

    Optmized Reducing Dishes solution

--- Daily_questions/Reducing_dishes.java
+++ Daily_questions/Reducing_dishes.java
@@ -4,31 +4,14 @@ import java.util.List;
 
 public class Reducing_dishes {
     public int maxSatisfaction(int[] satisfaction) {
-        // satisfaction = -Arrays.sort(-satisfaction);
         Arrays.sort(satisfaction);
-        int[] dp = new int[satisfaction.length];
-        List<Integer> dishes = new ArrayList<>();
-        dp[dp.length - 1] = satisfaction[satisfaction.length - 1];
-        dishes.add(dp.length - 1);
-        for (int i = satisfaction.length - 2; i >= 0; i--){
-            int max = maximumLikeTime(satisfaction, dp, dishes, i);
-            System.out.println(max);
-            if (max > dp[i + 1]){
-                dp[i] = max;
-                dishes.add(i);
-            } else {
-                dp[i] = dp[i + 1];
-            }
-        }
-        return dp[0] > 0 ? dp[0] : 0;
-    }
-    private int maximumLikeTime(int[] satisfaction, int[] dp, List<Integer> dishes, int index){
         int max = 0;
-        max += satisfaction[index] * 1;
-        int times = 1;
-        for (int i = dishes.size() - 1; i >= 0; i--){
-            times++;
-            max += satisfaction[dishes.get(i)] * times;
+        int runningTime = 0;
+        int sumSatisfactionDishes = 0;
+        for (int i = satisfaction.length - 1; i >= 0; i--){
+            sumSatisfactionDishes += satisfaction[i];
+            runningTime += sumSatisfactionDishes;
+            max = Math.max(max, runningTime);
         }
         return max;
     }
