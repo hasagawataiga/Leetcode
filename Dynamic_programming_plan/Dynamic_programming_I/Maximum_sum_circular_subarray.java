e5a147cbf535c4e490274ee1edbc159b4a4f7bea
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2023-04-06 13:02:57 +0200

    Maximum Sum Circular Subarray solution

---
+++ Dynamic_programming_plan/Dynamic_programming_I/Maximum_sum_circular_subarray.java
@@ -0,0 +1,23 @@
+package Dynamic_programming_plan.Dynamic_programming_I;
+
+public class Maximum_sum_circular_subarray {
+    public int maxSubarraySumCircular(int[] nums) {
+        int preSum = 0;
+        int preSumMax = 0;
+        int preSumMin = 0;
+        int max = nums[0];
+        int min = nums[0];
+        for (int i = 0; i < nums.length; i++){
+            preSum += nums[i];
+            preSumMax = Math.max(preSumMax, 0) + nums[i];
+            preSumMin = Math.min(preSumMin, 0) + nums[i];
+            min = Math.min(min, preSumMin);
+            max = Math.max(max, preSumMax);
+        }
+        if (min == preSum){
+            return max;
+        } else {
+            return Math.max(max, preSum - min);
+        }
+    }
+}
