0e63835a019d115e76255f8c8f8b5dfedc41765f
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2023-04-09 06:03:17 +0200

    Optimized question 1 solution

--- Contest/Contest340/Sum_of_distances.java
+++ Contest/Contest340/Sum_of_distances.java
@@ -6,36 +6,16 @@ import java.util.Map;
 public class Sum_of_distances {
     public long[] distance(int[] nums) {
         long[] res = new long[nums.length];
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++){
-            int num = nums[i];
-            if (map.containsKey(num)){
-                map.get(num).add(i);
-            } else {
-                map.put(num, new ArrayList<>());
-                map.get(num).add(i);
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int sum = 0; // Variable to store the sum of absolute differences
+            for (int j = 0; j < n; j++) {
+                if (nums[j] == nums[i] && j != i) {
+                    sum += Math.abs(i - j); // Calculate the absolute difference
+                }
             }
-        }
-//         for (int i = 0; i < nums.length; i++){
-            
-//         }
-        for (int i = 0; i < nums.length; i++){
-            if (map.get(nums[i]).size() == 1){
-                res[i] = 0;
-            } else {
-                res[i] = calc(map, i, nums[i]);                
-            }
+            res[i] = sum; // Store the sum in the result array
         }
         return res;
-    }
-    private long calc(Map<Integer, List<Integer>> map, int index, int val){
-        long sum = 0;
-        List<Integer> list = map.get(val);
-        for (int i = 0; i < list.size(); i++){
-           if (list.get(i) != index){
-               sum += (long)Math.abs(index - list.get(i));
-           }
-        }
-        return sum;
     }
 }
