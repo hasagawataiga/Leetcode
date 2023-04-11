f3a15f455ae6981e678564c9559ad42658ab2304
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2023-04-11 13:55:15 +0200

    optimized Remove Stars from a String solution

--- Daily_questions/Removing_stars_from_a_string.java
+++ Daily_questions/Removing_stars_from_a_string.java
@@ -1,25 +1,25 @@
-import java.util.Stack;
 
 public class Removing_stars_from_a_string {
     public String removeStars(String s) {
-        int i = 0;
-        Stack<Integer> stack = new Stack<>();
-        while (i < s.length()){
-            int ch = s.charAt(i) - 'a';
-            if (ch >= 0 && ch <= 26){
-                stack.push(ch);
+        int length = s.length();
+        char[] chars = new char[length];
+        int index = length - 1;
+        int i = index;
+        int countStars = 0;
+        while (i >= 0){
+            char ch = s.charAt(i);
+            if (ch == '*'){
+                countStars++;
             } else {
-                stack.pop();
+                if (countStars > 0){
+                    countStars--;
+                } else {
+                    chars[index] = ch;
+                    index--;
+                }
             }
-            i++;
+            i--;
         }
-        StringBuilder sb = new StringBuilder();
-        while (!stack.isEmpty()){
-            int num = stack.pop();
-            char ch = (char)(num + 'a');
-            sb.append(ch);
-        }
-
-        return sb.reverse().toString();
+        return new String(chars, index + 1, length - index - 1);
     }
 }
