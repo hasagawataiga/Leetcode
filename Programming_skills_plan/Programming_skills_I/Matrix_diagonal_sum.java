4e018982a81b81e423ef266b9ca7506d0dfc73ba
hasagawataiga <hoangvoletuan1603@gmail.com>
2023-04-18 18:20:18 +0200

    Matrix Diagonal Sum solution

---
+++ Programming_skills_plan/Programming_skills_I/Matrix_diagonal_sum.java
@@ -0,0 +1,17 @@
+package Programming_skills_plan.Programming_skills_I;
+
+public class Matrix_diagonal_sum {
+    public int diagonalSum(int[][] mat) {
+        int sum = 0;
+        int row = mat.length;
+        for (int i = 0; i < row; i++){
+            if (i + i == row - 1){
+                sum += mat[i][i];
+            } else {
+                sum += mat[i][i];
+                sum += mat[i][row - 1 - i];
+            }
+        }
+        return sum;
+    }
+}
