9a35bc633285f3f9837a870f65a61013f6656bb5
hasagawataiga <hoangvoletuan1603@gmail.com>
2023-04-21 16:44:17 +0200

    Next Greater Element I solution

---
+++ Programming_skills_plan/Programming_skills_I/Next_greater_elements_I.java
@@ -0,0 +1,22 @@
+package Programming_skills_plan.Programming_skills_I;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Next_greater_elements_I {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        for (int num : nums2){
+            while (!stack.isEmpty() && stack.peek() < num){
+                map.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+        for (int i = 0; i < nums1.length; i++){
+            nums1[i] = map.getOrDefault(nums1[i], -1);
+        }
+        return nums1;
+    }
+}
