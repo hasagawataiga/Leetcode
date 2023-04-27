fec3dfe6a34f1e2953e4def0c1c8e2135656a1ef
hasagawataiga <hoangvoletuan1603@gmail.com>
2023-04-27 11:34:46 +0200

    Optmized Best Time to Buy And Sell Stocks solution

--- Top_interview_150/Best_time_to_buy_and_sell_stocks.java
+++ Top_interview_150/Best_time_to_buy_and_sell_stocks.java
@@ -4,13 +4,13 @@ public class Best_time_to_buy_and_sell_stocks {
     public int maxProfit(int[] prices) {
         int profit = 0;
         int left = 0;
-        int right = 0;
+        int right = 1;
         while (right < prices.length){
-            int buy = prices[buy];
-            while (right < prices.length && prices[right] >= prices[left]){
-                right++;
+            if (prices[left] > prices[right]){
+                left = right;
             }
-            profit += prices[right]
+            profit = Math.max(profit, prices[right] - prices[left]);
+            right++;
         }
         return profit;
     }
