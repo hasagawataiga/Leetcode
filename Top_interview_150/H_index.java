485e1b4cb14f4d05bc9ac20fc81a8e8d79add390
hasagawataiga <hoangvoletuan1603@gmail.com>
2023-05-01 14:42:46 +0200

    Optimized H-index solution

--- Top_interview_150/H_index.java
+++ Top_interview_150/H_index.java
@@ -1,16 +1,18 @@
 package Top_interview_150;
 
-import java.util.Arrays;
-
 public class H_index {
     public int hIndex(int[] citations) {
-        Arrays.sort(citations);
         int n = citations.length;
         int res = 0;
-        for (int i = 0; i < n; i++){
-            int papers = n - i;
-            if (citations[i] >= papers){
-                res = papers;
+        int[] citationsCount = new int[n + 1];
+        for (int citation : citations){
+            citationsCount[Math.min(n, citation)]++;
+        }
+        int count = 0;
+        for (int i = n; i >= 0; i--){
+            count += citationsCount[i];
+            if (count >= i){
+                res = i;
                 break;
             }
         }
