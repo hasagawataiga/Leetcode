ed0c8d78fb906d1fdcbf2fe181086ee3a5ba058b
hasagawataiga <hoangvoletuan1603@gmail.com>
2023-05-01 19:12:44 +0200

    Optimized Product of Array Except Self solution

--- Top_interview_150/Product_of_array_except_self.java
+++ Top_interview_150/Product_of_array_except_self.java
@@ -5,20 +5,17 @@ package Top_interview_150;
 public class Product_of_array_except_self {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] products = new int[n];
-        int[] prefixs = new int[n];
-        prefixs[0] = 1;
+        // int[] products = new int[n];
         int[] suffixs = new int[n];
         suffixs[n - 1] = 1;
-        for (int i = 1; i < n; i++){
-            prefixs[i] = prefixs[i - 1] * nums[i - 1];
-        }
         for (int i = n - 2; i >=0; i--){
             suffixs[i] = suffixs[i + 1] * nums[i + 1];
         }
+        int prefix = 1;
         for (int i = 0; i < n; i++){
-            products[i] = prefixs[i] * suffixs[i];
+            suffixs[i] = prefix * suffixs[i];
+            prefix *= nums[i];
         }
-        return products;
+        return suffixs;
     }
 }
