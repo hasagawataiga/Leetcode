f24bd681dc7ceab009833bcde6be0a87ef4b0777
hasagawataiga <hoangvoletuan1603@gmail.com>
2023-05-02 19:06:18 +0200

    Optimized Gas Station solution

--- Daily_questions/Gas_station.java
+++ Daily_questions/Gas_station.java
@@ -1,37 +1,24 @@
+
+
+
+// Greedy approach
 public class Gas_station {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        if (gas.length == 1){
-            if (gas[0] >= cost[0]){
-                return 0;
-            } else {
+        int gasTank = 0;
+        int startPos = 0;
+        for (int i = 0; i < gas.length; i++){
+            gasTank += gas[i] - cost[i];
+            if (gasTank < 0){
+                startPos = i + 1;
+                gasTank = 0;
+            }
+        }
+        for (int i = 0; i <= startPos; i++){
+            gasTank += gas[i] - cost[i];
+            if (gasTank < 0){
                 return -1;
             }
         }
-        for (int i = 0; i < gas.length; i++){
-            if (gas[i] - cost[i] > 0){
-                if (isValid(gas, cost, i)){
-                    return i;
-                }
-            }
-        }
-        return -1;
-    }
-    private boolean isValid(int[] gas, int[] cost, int index){
-        int gasTank = 0;
-        for (int i = index; i < gas.length; i++){
-            gasTank += gas[i];
-            gasTank -= cost[i];
-            if (gasTank < 0){
-                return false;
-            }
-        }
-        for (int i = 0; i < index; i++){
-            gasTank += gas[i];
-            gasTank -= cost[i];
-            if (gasTank < 0){
-                return false;
-            }
-        }
-        return true;
+        return gasTank >= 0 ? startPos : -1;
     }
 }
