aee3b35baa5b34626f181d36275d471a7115e6a5
hasagawataiga <hoangvoletuan1603@gmail.com>
2023-05-04 10:59:47 +0200

    Dota2 Senate solution

---
+++ Daily_questions/Dota2_senate.java
@@ -0,0 +1,58 @@
+public class Dota2_senate {
+    public String predictPartyVictory(String senate) {
+        char[] senateChars = senate.toCharArray();
+        int n = senateChars.length;
+        int rCount = 0;
+        int dCount = 0;
+        for (char ch : senateChars){
+            if (ch == 'R'){
+                rCount++;
+            } else {
+                dCount++;
+            }
+        }
+        int index = 0;
+        String res = "";
+        while (rCount != 0 || dCount != 0){
+            if (senateChars[index] == 'R'){
+                if (!banNearestOpponent(senateChars, index, 'D')){
+                    res = "Radiant";
+                    return res;
+                } else {
+                    dCount--;
+                }
+            } else if (senateChars[index] == 'D'){
+                if (!banNearestOpponent(senateChars, index, 'R')){
+                    res = "Dire";
+                    return res;
+                } else {
+                    rCount--;
+                }
+            }
+            index++;
+            index %= n;
+            // System.out.print(index + ",");
+        }
+        // if (dCount > 0){
+        //     res = "Dire";
+        // } else {
+        //     res = "Radiant";
+        // }
+        return res;
+    }
+    private boolean banNearestOpponent(char[] senateChars, int index, char opponent){
+        for (int i = index + 1; i < senateChars.length; i++){
+            if (senateChars[i] == opponent){
+                senateChars[i] = 'X';
+                return true;
+            }
+        }
+        for (int i = 0; i < index; i++){
+            if (senateChars[i] == opponent){
+                senateChars[i] = 'X';
+                return true;
+            }
+        }
+        return false;
+    }
+}
