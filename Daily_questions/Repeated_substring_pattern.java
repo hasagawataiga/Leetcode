98dda42a031f8038193634e47abaf3b5bdcb72ec
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2023-05-10 23:34:24 +0200

    Repeated Substring Pattern solution

---
+++ Daily_questions/Repeated_substring_pattern.java
@@ -0,0 +1,31 @@
+
+
+public class Repeated_substring_pattern{
+    public boolean repeatedSubstringPattern(String s) {
+        int n = s.length();
+        char lastCh = s.charAt(n - 1);
+        int right = 0;
+        while (right < n){
+            int remainder = n % (right - left + 1);
+            while (right < n && s.charAt(right) != lastCh && remainder == 0){
+                remainder = n % (right - left + 1);
+                right++;
+            }
+            if (right >= n - 1){
+                return false;
+            }
+            String subString = s.substring(0, right + 1);
+            StringBuilder sb = new StringBuilder();
+            sb.append(subString);
+            int times = n / (right - left + 1);
+            for (int i = 1; i < times; i++){
+                sb.append(subString);
+            }
+            if (sb.toString().equals(s)){
+                return true;
+            }
+            right++;
+        }
+        return false;
+    }
+}
