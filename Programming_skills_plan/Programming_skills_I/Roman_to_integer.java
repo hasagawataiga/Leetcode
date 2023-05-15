502414024364e628c25fdd9077d132cc537c22f7
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2023-05-15 21:14:25 +0200

    Optimized Roman to Integer solution

--- Programming_skills_plan/Programming_skills_I/Roman_to_integer.java
+++ Programming_skills_plan/Programming_skills_I/Roman_to_integer.java
@@ -4,62 +4,39 @@ public class Roman_to_integer {
     public int romanToInt(String s) {
         int num = 0;
         int n = s.length();
-        for (int i = 0; i < n; i++){
+        int prev = 0;
+        int curr = 0;
+        for (int i = n - 1; i >= 0; i--){
             char ch = s.charAt(i);
-            if (ch == 'M'){
-                num += 1000;
-            } else if (ch == 'D'){
-                num += 500;
-            } else if (ch == 'C'){
-                if (i < n - 1){
-                    char nextCh = s.charAt(i + 1);
-                    if (nextCh == 'D'){
-                        num += 400;
-                        i++;
-                    } else if (nextCh == 'M'){
-                        num += 900;
-                        i++;
-                    } else {
-                        num += 100;
-                    }
-                } else {
-                    num += 100;
-                }
-            } else if (ch == 'L'){
-                num += 50;
-            } else if (ch == 'X'){
-                if (i < n - 1){
-                    char nextCh = s.charAt(i + 1);
-                    if (nextCh == 'L'){
-                        num += 40;
-                        i++;
-                    } else if (nextCh == 'C'){
-                        num += 90;
-                        i++;
-                    } else {
-                        num += 10;
-                    }
-                } else {
-                    num += 10;
-                }
-            } else if (ch == 'V'){
-                num += 5;
-            } else if (ch == 'I'){
-                if (i < n - 1){
-                    char nextCh = s.charAt(i + 1);
-                    if (nextCh == 'V'){
-                        num += 4;
-                        i++;
-                    } else if (nextCh == 'X'){
-                        num += 9;
-                        i++;
-                    } else {
-                        num += 1;
-                    }
-                } else {
-                    num += 1;
-                }
+            switch(ch){
+                case 'I':
+                    curr = 1;
+                    break;
+                case 'V':
+                    curr = 5;
+                    break;
+                case 'X':
+                    curr = 10;
+                    break;
+                case 'L':
+                    curr = 50;
+                    break;
+                case 'C':
+                    curr = 100;
+                    break;
+                case 'D':
+                    curr = 500;
+                    break;
+                case 'M':
+                    curr = 1000;
+                    break;
             }
+            if (curr < prev){
+                num -= curr;
+            } else {
+                num += curr;
+            }
+            prev = curr;
         }
         return num;
     }
