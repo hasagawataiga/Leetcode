3cebd87a22f575cbd396e921a4ef052dd54acdcf
hasagawataiga <hoangvoletuan1603@gmail.com>
2023-05-19 17:23:09 +0200

    Merge Two Sorted Lists solution

---
+++ Programming_skills_plan/Programming_skills_I/Merge_two_sorted_lists.java
@@ -0,0 +1,33 @@
+package Programming_skills_plan.Programming_skills_I;
+
+import interview_questions.linked_list.ListNode;
+
+public class Merge_two_sorted_lists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode node = new ListNode();
+        if (list1 == null && list2 == null){
+            return null;
+        }
+        if (list1 == null){
+            node = list2;
+            list2 = list2.next;
+            node.next = mergeTwoLists(list1, list2);
+            return node;
+        }
+        if (list2 == null){
+            node = list1;
+            list1 = list1.next;
+            node.next = mergeTwoLists(list1, list2);
+            return node;
+        }
+        if (list1.val < list2.val){
+            node = list1;
+            list1 = list1.next;
+        } else {
+            node = list2;
+            list2 = list2.next;
+        }
+        node.next = mergeTwoLists(list1, list2);
+        return node;
+    }
+}
