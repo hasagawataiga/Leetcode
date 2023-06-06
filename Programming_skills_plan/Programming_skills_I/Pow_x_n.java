91d157269937b3a6fc826b9a1846aefc45d79691
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2023-06-06 22:53:02 +0200

    Pow(x, n) solution

--- Programming_skills_plan/Programming_skills_I/Pow_x_n.java
+++ Programming_skills_plan/Programming_skills_I/Pow_x_n.java
@@ -1,23 +1,23 @@
 package Programming_skills_plan.Programming_skills_I;
 
 public class Pow_x_n {
-    public double myPow(double x, int n) {
-        double res = 1;
+    public double myPow(double x, int n){
+        return binaryExp(x, (long) n);
+    }
+    public double binaryExp(double x, long n) {
+        // Base case
         if (n == 0){
-            return res;
+            return 1;
         }
-        long absN = Math.abs((long)n);
-        while (absN > 0){
-            if ((absN & 1) == 1){
-                res *= x;
-            }
-            x *= x;
-            absN >>= 1;
+        // Ignore n < 0
+        if (n < 0){
+            return 1.0 / binaryExp(x, -1 * n);
         }
-        if (n > 0){
-            return res;
+        // If n is odd, x^n = (x * x) ^ (n - 1) / 2, normal otherwise
+        if (n % 2 == 1){
+            return x * binaryExp(x * x, (n - 1) / 2);
         } else {
-            return 1/res;
+            return binaryExp(x * x, n / 2);
         }
     }
 }
