3fb5c19fb6288479e9d01a59a992ec847919562e
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2023-06-06 10:33:46 +0200

    Can Make Arithmetic Progression From Sequence solution

---
+++ Daily_questions/Can_make_arithmetic_progression_from_sequence.java
@@ -0,0 +1,37 @@
+public class Can_make_arithmetic_progression_from_sequence {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        int length = arr.length;
+        int min = arr[0];
+        int max = arr[0];
+        for (int i = 1; i < length; i++){
+            min = Math.min(min, arr[i]);
+            max = Math.max(max, arr[i]);
+        }
+        if (max == min){
+            return true;
+        }
+        if ((max - min)%(length - 1) != 0){
+            return false;
+        }
+        int diff = (max - min)/(length - 1);
+        int i = 0;
+        while (i < length){
+            if ((arr[i] - min)%diff != 0){
+                return false;
+            } else {
+                int j = (arr[i] - min)/diff;
+                if (i == j){
+                    i++;
+                    continue;
+                } else if (arr[i] == arr[j]){
+                    return false;
+                } else {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }       
+            }
+        }
+        return true;
+    }
+}
