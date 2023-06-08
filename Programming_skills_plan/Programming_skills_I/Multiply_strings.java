15143671419344f55d6a2aa39d7233d422ef6b27
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2023-06-08 20:27:44 +0200

    Optimized Multiply Strings solution

--- Programming_skills_plan/Programming_skills_I/Multiply_strings.java
+++ Programming_skills_plan/Programming_skills_I/Multiply_strings.java
@@ -3,74 +3,34 @@ package Programming_skills_plan.Programming_skills_I;
 
 public class Multiply_strings{
     public String multiply(String num1, String num2) {
-        String res = "0";
-        StringBuilder sb = new StringBuilder();
-        for (int i = num2.length() - 1; i >= 0; i--){
-            int num = num2.charAt(i) - '0';
-            // if (num == 0){
-            //     sb.append("0");
-            //     continue;
-            // }
-            String product = multiplyString(num1, num);
-            if (product.equals("0")){
-                sb.append("0");
-                continue;
-            }
-            product += sb.toString();
-            res = plusString(product, res);
-            sb.append("0");
-        }
-        return res;
-    }
-    private String plusString(String num1, String num2){
-        int i = num1.length() - 1;
-        int j = num2.length() - 1;
-        int extra = 0;
-        StringBuilder sb = new StringBuilder();
-        while (i >= 0 && j >= 0){
-            int num = (num1.charAt(i) - '0') + (num2.charAt(j) - '0') + extra;
-            extra = num / 10;
-            num %= 10;
-            i--;
-            j--;
-            sb.append(num);
-        }
-        while (i >= 0){
-            int num = num1.charAt(i) - '0' + extra;
-            extra = num / 10;
-            num = num % 10;
-            i--;
-            sb.append(num);
-        }
-        while (j >= 0){
-            int num = num2.charAt(j) - '0' + extra;
-            extra = num / 10;
-            num = num % 10;
-            j--;
-            sb.append(num);
-        }
-        if (extra > 0){
-            sb.append(extra);
-        }
-        return sb.reverse().toString();
-    }
-    private String multiplyString(String num1, int num2){
-        if (num2 == 0){
+        if (num1.equals("0") || num2.equals("0")){
             return "0";
         }
-        int i = num1.length() - 1;
-        int extra = 0;
+        if (num1.equals("1")){
+            return num2;
+        }
+        if (num2.equals("1")){
+            return num1;
+        }
+        int n1 = num1.length();
+        int n2 = num2.length();
+        int[] num = new int[n1 + n2];
         StringBuilder sb = new StringBuilder();
-        while (i >= 0){
-            int num = (num1.charAt(i) - '0') * num2 + extra;
-            extra = num / 10;
-            num %= 10;
-            i--;
-            sb.append(num);
+        for (int i = n1 - 1; i >= 0; i--){
+            for (int j = n2 - 1; j >= 0; j--){
+                int val1 = num1.charAt(i) - '0';
+                int val2 = num2.charAt(j) - '0';
+                int product = val1 * val2 + num[i + j + 1];
+                num[i + j + 1] = product % 10;
+                num[i + j] += product / 10; 
+            }
         }
-        if (extra > 0){
-            sb.append(extra);
+        for (int i = 0; i < num.length; i++){
+            if (num[i] == 0 && sb.isEmpty()){
+                continue;
+            }
+            sb.append(num[i]);
         }
-        return sb.reverse().toString();
+        return sb.toString();
     }
 }
