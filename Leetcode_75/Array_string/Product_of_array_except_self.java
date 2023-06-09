66cbdc0d4a605cbfda3ebc85599a1fcb977bf4ff
hasagawataiga <hoangvoletuan1603@gmail.com>
2023-06-09 20:12:40 +0200

    Product Of Array Except Self solution

---
+++ Leetcode_75/Array_string/Product_of_array_except_self.java
@@ -0,0 +1,19 @@
+package Leetcode_75.Array_string;
+
+public class Product_of_array_except_self {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        // int[] products = new int[n];
+        int[] suffixs = new int[n];
+        suffixs[n - 1] = 1;
+        for (int i = n - 2; i >=0; i--){
+            suffixs[i] = suffixs[i + 1] * nums[i + 1];
+        }
+        int prefix = 1;
+        for (int i = 0; i < n; i++){
+            suffixs[i] = prefix * suffixs[i];
+            prefix *= nums[i];
+        }
+        return suffixs;
+    }
+}
