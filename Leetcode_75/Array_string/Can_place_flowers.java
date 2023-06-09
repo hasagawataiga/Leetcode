d4cbf4e7693e374bab6531df1a4f2d157e776268
hasagawataiga <hoangvoletuan1603@gmail.com>
2023-06-09 19:01:52 +0200

    Can Place Flowers solution

---
+++ Leetcode_75/Array_string/Can_place_flowers.java
@@ -0,0 +1,24 @@
+package Leetcode_75.Array_string;
+
+public class Can_place_flowers {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        for (int i = 0; i < flowerbed.length; i++){
+            if (n == 0){
+                return true;
+            }
+            if (flowerbed[i] == 1){
+                i++;
+            } else {
+                if (i + 1 < flowerbed.length){
+                    if (flowerbed[i + 1] == 0){
+                        n--;
+                        i++;
+                    }
+                } else {
+                    n--;
+                }
+            }
+        }
+        return n == 0 ? true : false;
+    }
+}
