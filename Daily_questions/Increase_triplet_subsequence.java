5cf8158f28ec0058f2ed4f0be4932ff7881cee11
hasagawataiga <hoangvoletuan1603@gmail.com>
2023-06-12 14:06:34 +0200

    Optimized Increase Triplet Subsequence solution

--- Daily_questions/Increase_triplet_subsequence.java
+++ Daily_questions/Increase_triplet_subsequence.java
@@ -1,23 +1,19 @@
 public class Increase_triplet_subsequence {
     public boolean increasingTriplet(int[] nums) {
-        int n = nums.length;
-        if (n < 3){
+        if (nums.length < 3) {
             return false;
         }
-        // stores the max
-        // stores the min
-        int[] prefix = new int[n];
-        prefix[0] = nums[0];
-        int[] suffix = new int[n];
-        suffix[n - 1] = nums[n - 1];
-        for (int i = 1; i < n; i++){
-            prefix[i] = Math.min(prefix[i - 1], nums[i]);
-        }
-        for (int i = n - 2; i >= 1; i--){
-            if (nums[i] < suffix[i + 1] && nums[i] > prefix[i - 1]){
+        int val1 = Integer.MAX_VALUE;
+        int val2 = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num <= val1) {
+                val1 = num;
+            } else if (num <= val2) {
+                val2 = num;
+            } else {
                 return true;
             }
-            suffix[i] = Math.max(suffix[i + 1], nums[i]);
         }
         return false;
     }
