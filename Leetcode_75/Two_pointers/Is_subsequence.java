80d2ede9303880612546f5236bfdc91ca6fa882b
hasagawataiga <hoangvoletuan1603@gmail.com>
2023-06-14 16:50:49 +0200

    Is Subsequence solution

---
+++ Leetcode_75/Two_pointers/Is_subsequence.java
@@ -0,0 +1,24 @@
+package Leetcode_75.Two_pointers;
+
+public class Is_subsequence {
+    public boolean isSubsequence(String s, String t) {
+        int lengthS = s.length();
+        int lengthT = t.length();
+        if (lengthS == 0){
+            return true;
+        }
+        if (lengthS > lengthT){
+            return false;
+        }
+        int count = 0;
+        for (int i = 0; i < lengthT; i++){
+            if (s.charAt(count) == t.charAt(i)){
+                count++;
+            }
+            if (count == lengthS){
+                return true;
+            }
+        }
+        return false;
+    }
+}
