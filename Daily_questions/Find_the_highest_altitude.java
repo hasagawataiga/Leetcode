36dc648646443dc6c6e94fd80c51dcc008fbc198
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2023-06-19 06:48:16 +0200

    Find The Highest Altitude solution

---
+++ Daily_questions/Find_the_highest_altitude.java
@@ -0,0 +1,12 @@
+
+public class Find_the_highest_altitude{
+    public int largestAltitude(int[] gain) {
+        int prefix = 0;
+        int max = 0;
+        for (int num : gain){
+            prefix += num;
+            max = Math.max(max, prefix);
+        }
+        return max;
+    }
+}
