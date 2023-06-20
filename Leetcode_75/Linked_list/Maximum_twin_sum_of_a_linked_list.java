92829df47ee00d4db34fa1b21d39d7613f405106
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2023-06-20 19:10:03 +0200

    Maximum Twin Sum of a Linked List solution

---
+++ Leetcode_75/Linked_list/Maximum_twin_sum_of_a_linked_list.java
@@ -0,0 +1,25 @@
+package Leetcode_75.Linked_list;
+
+import java.util.Stack;
+
+import interview_questions.linked_list.ListNode;
+
+public class Maximum_twin_sum_of_a_linked_list {
+    public int pairSum(ListNode head) {
+        int sum = 0;
+        ListNode slow = head;
+        ListNode fast = head;
+        Stack<Integer> stack = new Stack<>();
+        while (fast != null && fast.next != null){
+            stack.add(slow.val);
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        while (slow != null && !stack.isEmpty()){
+            int twin = stack.pop();
+            sum = Math.max(sum, twin + slow.val);
+            slow = slow.next;
+        }
+        return sum;
+    }
+}
