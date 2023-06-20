891fa4418f47dc4cb843e47f93a0c4bd980c043a
hasagawataiga <hoangvoletuan1603@gmail.com>
2023-06-20 11:55:13 +0200

    Optimized K Radius Subarray Averages solution

--- Daily_questions/K_radius_subarray_averages.java
+++ Daily_questions/K_radius_subarray_averages.java
@@ -4,29 +4,25 @@ public class K_radius_subarray_averages {
     public int[] getAverages(int[] nums, int k) {
         long prefix = 0;
         int n = nums.length;
+        int[] res = new int[n];
+        Arrays.fill(res, -1);
         if (n < 2 * k + 1){
-            int[] res = new int[n];
-            Arrays.fill(res, -1);
             return res;
         }
         int right = 0;
         int left = 0;
-        int curr = 0;
         int range = 2 * k + 1;
-        int[] res = new int[n];
         while (right < range){
             prefix += nums[right++];
         }
-        while (curr < n){
-            if (curr >= k && curr < n - k){
-                res[curr++] = (int) (prefix / range);
-                if (right < n){
-                    prefix += nums[right++];
-                }
-                prefix -= nums[left++];
-            } else {
-                res[curr++] = -1;
+        int curr = k;
+        res[curr++] = (int) (prefix / range);
+        while (curr < n - k){
+            if (right < n){
+                prefix += nums[right++];
             }
+            prefix -= nums[left++];
+            res[curr++] = (int) (prefix / range);
         }
         return res;
     }
