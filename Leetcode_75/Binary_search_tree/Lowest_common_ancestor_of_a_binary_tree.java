98a2b68d01603682ce9eb66f6eb633de75e8eab9
hasagawataiga <hoangvoletuan1603@gmail.com>
2023-06-22 12:31:45 +0200

    Optimized Lowest Common Ancestor of a Binary Tree solution

--- Leetcode_75/Binary_search_tree/Lowest_common_ancestor_of_a_binary_tree.java
+++ Leetcode_75/Binary_search_tree/Lowest_common_ancestor_of_a_binary_tree.java
@@ -4,12 +4,20 @@ import interview_questions.trees.TreeNode;
 
 public class Lowest_common_ancestor_of_a_binary_tree{
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root.val < p.val && root.val < q.val){
-            return lowestCommonAncestor(root.right, p, q);
-        } else if (root.val > p.val && root.val > q.val){
-            return lowestCommonAncestor(root.left, p, q);
-        } else {
+        if (root == null || root == p || root == q){
             return root;
         }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null){
+            return root;
+        }
+        if (left == null){
+            return right;
+        }
+        if (right == null){
+            return left;
+        }
+        return root;
     }
 }
