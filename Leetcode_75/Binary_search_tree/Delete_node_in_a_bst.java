9c4e38b85726e500e7eafa8531f56eec1214fdd0
hasagawataiga <hoangvoletuan1603@gmail.com>
2023-06-22 18:51:25 +0200

    Delete Node in a BST solution

---
+++ Leetcode_75/Binary_search_tree/Delete_node_in_a_bst.java
@@ -0,0 +1,34 @@
+package Leetcode_75.Binary_search_tree;
+
+import interview_questions.trees.TreeNode;
+
+public class Delete_node_in_a_bst {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null){
+            return root;
+        }
+        if (key < root.val){
+            root.left = deleteNode(root.left, key);
+        } else if (key > root.val){
+            root.right = deleteNode(root.right, key);
+        } else {
+            if (root.left == null){
+                return root.right;
+            }
+            if (root.right == null){
+                return root.left;
+            }
+
+            TreeNode successor = findSuccessor(root.right);
+            root.val = successor.val;
+            root.right = deleteNode(root.right, successor.val);
+        }
+        return root;
+    }
+    private TreeNode findSuccessor(TreeNode node){
+        while (node.left != null){
+            node = node.left;
+        }
+        return node;
+    }
+}
