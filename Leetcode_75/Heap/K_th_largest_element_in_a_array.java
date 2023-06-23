43288ba57647d30c8a2b496768c1d27f7e168eff
hasagawataiga <hoangvoletuan1603@gmail.com>
2023-06-23 20:20:23 +0200

    Optimized K-th Largest Element in a Array solution

--- Leetcode_75/Heap/K_th_largest_element_in_a_array.java
+++ Leetcode_75/Heap/K_th_largest_element_in_a_array.java
@@ -3,14 +3,33 @@ package Leetcode_75.Heap;
 import java.util.PriorityQueue;
 
 public class K_th_largest_element_in_a_array {
+    // public int findKthLargest(int[] nums, int k) {
+    //     PriorityQueue<Integer> heap = new PriorityQueue<>();
+    //     for (int num : nums){
+    //         heap.add(num);
+    //         if (heap.size() > k){
+    //             heap.remove();
+    //         }
+    //     }
+    //     return heap.peek();
+    // }
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        int min = nums[0];
+        int max = nums[0];
         for (int num : nums){
-            heap.add(num);
-            if (heap.size() > k){
-                heap.remove();
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+        int[] count = new int[max - min + 1];
+        for (int num : nums){
+            count[num - min]++;
+        }
+        for (int i = count.length - 1; i >= 0; i--){
+            k -= count[i];
+            if (k <= 0){
+                return i + min;
             }
         }
-        return heap.peek();
+        return -1;
     }
 }
