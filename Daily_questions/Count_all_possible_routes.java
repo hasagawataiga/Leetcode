31de001edc86ab6bbec258f90c53371ee6e3c2ef
hasagawataiga <hoangvoletuan1603@gmail.com>
2023-06-25 12:15:19 +0200

    Count All Possible Routes solution

---
+++ Daily_questions/Count_all_possible_routes.java
@@ -0,0 +1,38 @@
+public class Count_all_possible_routes {
+    int mod = 1_000_000_007;
+    int count = 0;
+    int n;
+    public int countRoutes(int[] locations, int start, int finish, int fuel) {
+        this.n = locations.length;
+        int[][] dp = new int[n][fuel + 1];
+        boolean[][] visited = new boolean[n][fuel + 1];
+        travel(locations, dp, visited, start, finish, fuel);
+        return dp[start][fuel];
+    }
+    private void travel(int[] locations, int[][] dp, boolean[][] visited, int index, int end, int fuel){
+        if (visited[index][fuel]){
+            return;
+        }
+        visited[index][fuel] = true;
+        
+        int temp = 0;
+        if (index == end){
+            temp++;
+        }
+        if (fuel == 0){
+            dp[index][fuel] = temp;
+            return;
+        }
+        for (int i = 0; i < n; i++){
+            if (i == index){
+                continue;
+            }
+            int fuelUsed = Math.abs(locations[index] - locations[i]);
+            if (fuel - fuelUsed >= 0){
+                travel(locations, dp, visited, i, end, fuel - fuelUsed);
+                temp = (temp + dp[i][fuel - fuelUsed]) % mod;
+            }
+        }
+        dp[index][fuel] = temp;
+    }
+}
