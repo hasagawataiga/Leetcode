3e6e1bf8c88e1449707a2af124a7de24f8f1be37
hasagawataiga <hoangvoletuan1603@gmail.com>
2023-07-07 22:29:15 +0200

    Optimized Maximize the Confusion of an Exam solution

--- Daily_questions/Maximize_the_confusion_of_an_exam.java
+++ Daily_questions/Maximize_the_confusion_of_an_exam.java
@@ -1,46 +1,48 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Maximize_the_confusion_of_an_exam {
     public int maxConsecutiveAnswers(String answerKey, int k) {
-        int trues = 0;
-        int falses = 0;
         int n = answerKey.length();
-        if (n == k){
-            return n;
-        }
-        if (n == 2){
-            if (answerKey.charAt(0) != answerKey.charAt(1)){
-                if (k > 0){
-                    return 2;
-                } else {
-                    return 1;
-                }
-            } else {
-                return 2;
-            }
-        }
-        int[] mins = new int[n];
-        for (int i = 0; i < n; i++){
-            char ch = answerKey.charAt(i);
-            if (ch == 'T'){
-                trues++;
-            } else {
-                falses++;
-            }
-            mins[i] = Math.min(trues, falses);
-        }
-        int left = 1;
-        int right = n - 1;
-        while (left < right){
-            int mid = left + (right - left) / 2;
-            if (mins[mid] <= k){
-                left = mid + 1;
+        int left = k, right = n;
+        
+        while (left < right) {
+            int mid = (left + right + 1) / 2;
+            
+            if (isValid(answerKey, mid, k)) {
+                left = mid;
             } else {
                 right = mid - 1;
             }
-            System.out.println(mins[mid]);
         }
-        while (left < n && answerKey.charAt(left - 1) == answerKey.charAt(left)){
-            left++;
-        }
+        
         return left;
+    }
+    
+    private boolean isValid(String answerKey, int size, int k) {
+        int n = answerKey.length();
+        Map<Character, Integer> counter = new HashMap<>();
+        
+        for (int i = 0; i < size; i++) {
+            char c = answerKey.charAt(i);
+            counter.put(c, counter.getOrDefault(c, 0) + 1);
+        }
+        
+        if (Math.min(counter.getOrDefault('T', 0), counter.getOrDefault('F', 0)) <= k) {
+            return true;
+        }
+        
+        for (int i = size; i < n; i++) {
+            char c1 = answerKey.charAt(i);
+            counter.put(c1, counter.getOrDefault(c1, 0) + 1);
+            char c2 = answerKey.charAt(i - size);
+            counter.put(c2, counter.getOrDefault(c2, 0) - 1);
+            
+            if (Math.min(counter.getOrDefault('T', 0), counter.getOrDefault('F', 0)) <= k) {
+                return true;
+            }
+        }
+        
+        return false;
     }
 }
