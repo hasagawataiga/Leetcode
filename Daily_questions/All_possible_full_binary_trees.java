d730923f863d98077501df584278ed657de28dbc
hasagawataiga <hoangvoletuan1603@gmail.com>
2023-07-24 22:18:13 +0200

    All Possible Full Binary Trees solution

---
+++ Daily_questions/All_possible_full_binary_trees.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+
+public class All_possible_full_binary_trees {
+    public class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(){}
+        TreeNode(int val){this.val = val;}
+        TreeNode(int val, TreeNode left, TreeNode right){
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    private Map<Integer, List<TreeNode>> memo = new HashMap<>();
+
+    public List<TreeNode> allPossibleFBT(int n) {
+        if (n % 2 == 0) {
+            return new ArrayList<TreeNode>();
+        }
+
+        if (n == 1) {
+            return Arrays.asList(new TreeNode());
+        }
+
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+
+        List<TreeNode> res = new ArrayList<TreeNode>();
+        for (int i = 1; i < n; i += 2) {
+            List<TreeNode> left = allPossibleFBT(i);
+            List<TreeNode> right = allPossibleFBT(n - i - 1);
+
+            for (TreeNode l : left) {
+                for (TreeNode r : right) {
+                    TreeNode root = new TreeNode(0, l, r);
+                    res.add(root);
+                }
+            }
+        }
+
+        memo.put(n, res);
+        return res;
+    }
+}
