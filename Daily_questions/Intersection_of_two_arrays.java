3f237c2e9ec957c4a6ff93a371c6cff69512a644
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2024-03-10 23:42:28 +0100

    Improve Intersection of Two Arrays solution

--- Daily_questions/Intersection_of_two_arrays.java
+++ Daily_questions/Intersection_of_two_arrays.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Intersection_of_two_arrays {
     public int[] intersection(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
@@ -26,4 +32,24 @@ public class Intersection_of_two_arrays {
         }
         return res.stream().mapToInt(Integer::intValue).toArray();
     }
+
+    // Alternative Approach consumes more memory but faster
+    // public int[] intersection(int[] nums1, int[] nums2) {
+    //     List<Integer> res = new ArrayList<>();
+    //     Set<Integer> container = new HashSet<>();
+    //     for (int num : nums1) {
+    //         container.add(num);
+    //     }
+    //     for (int num : nums2) {
+    //         if (container.contains(num)) {
+    //             res.add(num);
+    //             container.remove(num);
+    //         }
+    //     }
+    //     int[] arrayRes = new int[res.size()];
+    //     for (int i = 0; i < res.size(); i++) {
+    //         arrayRes[i] = res.get(i);
+    //     }
+    //     return arrayRes;
+    // }
 }
