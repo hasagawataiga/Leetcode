1393503c6bdf1297ffb1ffc938942967c42e7d43
hasagawataiga <hoangvoletuan1603@gmail.com>
2024-03-20 00:17:28 +0100

    TaskSchedule solution update

---
+++ Daily_questions/Task_schedules.java
@@ -0,0 +1,27 @@
+public class Task_schedules {
+            // Counter array to store the frequency of each task
+            int[] counter = new int[26];
+            int maximum = 0;
+            int maxCount = 0;
+    
+            // Traverse through tasks to calculate task frequencies
+            for (char task : tasks) {
+                counter[task - 'A']++;
+                if (maximum == counter[task - 'A']) {
+                    maxCount++;
+                }
+                else if (maximum < counter[task - 'A']) {
+                    maximum = counter[task - 'A'];
+                    maxCount = 1;
+                }
+            }
+            
+            // Calculate idle slots, available tasks, and idles needed
+            int partCount = maximum - 1;
+            int partLength = n - (maxCount - 1);
+            int emptySlots = partCount * partLength;
+            int availableTasks = tasks.length - maximum * maxCount;
+            int idles = Math.max(0, emptySlots - availableTasks);
+            
+            return tasks.length + idles;
+}
