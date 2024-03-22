1b6aaa15d0e27952a767925d3070bd2d1e716b26
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2024-03-22 12:00:05 +0100

    Reverse Linked List solution

---
+++ Daily_questions/Reverse_linked_list.java
@@ -0,0 +1,13 @@
+public class Reverse_linked_list {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+}
