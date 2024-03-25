5b564e54fb99895c082cb37a720900a1d014184e
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2024-03-25 18:46:20 +0100

    Counting Bits solution

---
+++ Leetcode_75/Bit_manipulation/Counting_bits.java
@@ -0,0 +1,10 @@
+public class Counting_bits {
+    public int[] countBits(int n) {
+        int[] res = new int[n + 1];
+        res[0] = 0;
+        for (int i = 1; i < n + 1; i++){
+            res[i] = res[i >> 1] + (i & 1);
+        }
+        return res;
+    }
+}
