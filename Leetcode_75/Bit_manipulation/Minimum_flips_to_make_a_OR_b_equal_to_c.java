c074a68bf11bf8ef39a78cc9a90b4c2bd1581353
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2024-03-25 19:04:38 +0100

    Fix algorithm bug

--- Leetcode_75/Bit_manipulation/Minimum_flips_to_make_a_OR_b_equal_to_c.java
+++ Leetcode_75/Bit_manipulation/Minimum_flips_to_make_a_OR_b_equal_to_c.java
@@ -3,7 +3,7 @@ package Leetcode_75.Bit_manipulation;
 public class Minimum_flips_to_make_a_OR_b_equal_to_c {
     public int minFlips(int a, int b, int c) {
         int count = 0;
-        while (a != 0 && b != 0 && c != 0) {
+        while (a != 0 || b != 0 || c != 0) {
             if ((c & 1) == 1) {
                 if ((a & 1) == 0 && (b & 1) == 0) {
                     count++;
