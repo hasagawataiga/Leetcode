afd75588c29eca668dfabb6d92421cba95afa58d
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2024-03-25 19:03:11 +0100

    Minimum Flips to Make A OR B Equal to C solution

---
+++ Leetcode_75/Bit_manipulation/Minimum_flips_to_make_a_OR_b_equal_to_c.java
@@ -0,0 +1,20 @@
+package Leetcode_75.Bit_manipulation;
+
+public class Minimum_flips_to_make_a_OR_b_equal_to_c {
+    public int minFlips(int a, int b, int c) {
+        int count = 0;
+        while (a != 0 && b != 0 && c != 0) {
+            if ((c & 1) == 1) {
+                if ((a & 1) == 0 && (b & 1) == 0) {
+                    count++;
+                }
+            } else {
+                count += (a & 1) + (b & 1);
+            }
+            a >>= 1;
+            b >>= 1;
+            c >>= 1;
+        }
+        return count;
+    }
+}
