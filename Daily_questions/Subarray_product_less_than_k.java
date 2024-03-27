543cf2e8c3535b213fe3baff35d272bdb4c1736c
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2024-03-27 16:27:54 +0100

    Adjust solution

--- Daily_questions/Subarray_product_less_than_k.java
+++ Daily_questions/Subarray_product_less_than_k.java
@@ -1,15 +1,20 @@
 public class Subarray_product_less_than_k {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k < 2) {
+            return 0;
+        }
         int res = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int product = 1;
-            for (int j = i; j < nums.length; j++) {
-                product *= nums[j];
-                if (product >= k) {
-                    break;
-                }
-                res++;
+        int first = 0;
+        int second = 0;
+        int product = 1;
+        while (second < nums.length) {
+            product *= nums[second];
+            while (product >= k) {
+                product /= nums[first];
+                first++;
             }
+            res += second - first + 1;
+            second++;
         }
         return res;
     }
