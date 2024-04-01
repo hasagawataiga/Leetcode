3e2f3ff99c9588da78d9aa3308a408f31aa234f0
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2024-04-01 11:03:04 +0200

    Length of Last Word solution

--- Daily_questions/Length_of_last_word.java
+++ Daily_questions/Length_of_last_word.java
@@ -3,4 +3,21 @@ public class Length_of_last_word {
         String[] strs = s.split(" ");
         return strs[strs.length - 1].length();
     }
+    // Second approach
+    // public int lengthOfLastWord(String s) {
+    //     int length = s.length();
+    //     int start = length - 1;
+    //     for (; start >= 0; start--) {
+    //         if (s.charAt(start) != ' ') {
+    //             break;
+    //         }
+    //     }
+    //     int end = start;
+    //     for (; start >= 0; start--) {
+    //         if (s.charAt(start) == ' ') {
+    //             break;
+    //         }
+    //     }
+    //     return end - start;
+    // }
 }
