f8e994f7dda3aa3b85564a9f758f03ea0dc5f208
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2024-04-10 09:14:59 +0200

    Approach updated

--- Daily_questions/Domino_and_tromino_titling.java
+++ Daily_questions/Domino_and_tromino_titling.java
@@ -1,29 +1,37 @@
 public class Domino_and_tromino_titling {
     int MOD = 1_000_000_007;
     public int numTilings(int n) {
-        int[][] dp = new int[n][3];
+        int[] dp = new int[n];
         traversal(n - 1, dp);
-        return dp[n - 1][0];
+        return dp[n - 1];
     }
-    private void traversal(int index, int[][] dp) {
+    private void traversal(int index, int[] dp) {
         if (index == 0) {
-            dp[index][0] = 1;
+            dp[index] = 1;
             return;
         }
         if (index == 1) {
-            dp[index][0] = 2;
-            dp[index][1] = 1;
-            dp[index][2] = 1;
+            dp[index] = 2;
             return;
         }
-        if (dp[index][0] != 0) {
+        if (index == 2) {
+            dp[index] = 5;
+            return;
+        }
+        if (dp[index] != 0) {
             return;
         }
         traversal(index - 1, dp);
-        traversal(index - 2, dp);
-        dp[index][0] = ((dp[index - 1][0] + dp[index - 1][1]) % MOD + (dp[index - 1][2] + dp[index - 2][0]) % MOD) % MOD;
-        dp[index][1] = (dp[index - 1][2] + dp[index - 2][0]) % MOD;
-        dp[index][2] = (dp[index - 1][1] + dp[index - 2][0]) % MOD;
+        traversal(index - 3, dp);
+        dp[index] = (2 * dp[index - 1] % MOD + dp[index -  3]) % MOD;
         return;
     }
 }
+
+// Second Approac
+// For this problem:
+// dp[n] = dp[n-1] + dp[n-2] + 2 * (dp[n-3] + ... + dp[0]) -- E1
+// dp[n-1] = dp[n-2] + dp[n-3] + 2 * (dp[n-4] + ... + dp[0]) -- E2
+// E1 - E2:
+// dp[n] - dp[n-1] = dp[n-1] + dp[n-3]
+// --> dp[n] = 2*dp[n-1] + dp[n-3]
