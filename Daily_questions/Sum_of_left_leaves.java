f3a8645c9e55ff8a15e6aed1306d040ea5bcf6f2
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2024-04-14 14:00:38 +0200

    Sum of Left Leaves solution

---
+++ Daily_questions/Sum_of_left_leaves.java
@@ -0,0 +1,17 @@
+public class Sum_of_left_leaves {
+    int sum = 0;
+    public int sumOfLeftLeaves(TreeNode root) {
+        traversal(root, false);
+        return sum;
+    }
+    private void traversal(TreeNode node, boolean isLeft) {
+        if (node == null) {
+            return;
+        }
+        if (node.left == null && node.right == null && isLeft) {
+            sum += node.val;
+        }
+        traversal(node.left, true);
+        traversal(node.right, false);
+    }
+}
