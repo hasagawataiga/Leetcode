c29ea890701e90c091b195e490185a5c524b5eef
hasagawataiga <hoangvoletuan1603@gmail.com>
2024-04-16 23:52:04 +0200

    Add One Row to Tree solution

---
+++ Daily_questions/Add_one_row_to_tree.java
@@ -0,0 +1,28 @@
+import interview_questions.trees.TreeNode;
+
+public class Add_one_row_to_tree {
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if (depth == 1) {
+            TreeNode node = new TreeNode(val, root, null);
+            return node;
+        }
+        traversal(root, depth, val);
+        return root;
+    }
+    private void traversal(TreeNode node, int depth, int val) {
+        if (node == null) {
+            return;
+        }
+        if (depth == 2) {
+            TreeNode left = node.left;
+            TreeNode right = node.right;
+            TreeNode targetLeft = new TreeNode(val, left, null);
+            TreeNode targetRight = new TreeNode(val, null, right);
+            node.left = targetLeft;
+            node.right = targetRight;
+            return;
+        }
+        traversal(node.left, depth - 1, val);
+        traversal(node.right, depth - 1, val);
+    }
+}
