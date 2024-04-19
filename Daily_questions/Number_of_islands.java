846e1e00b495ce3e0f68ef39557beedbaa1a45f2
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2024-04-19 09:48:46 +0200

    Adjust Number of Islands solution

--- Daily_questions/Number_of_islands.java
+++ Daily_questions/Number_of_islands.java
@@ -1,15 +1,13 @@
 public class Number_of_islands {
     int rows;
     int cols;
-    boolean[][] isChecked;
     public int numIslands(char[][] grid) {
         rows = grid.length;
         cols = grid[0].length;
-        isChecked = new boolean[rows][cols];
         int count = 0;
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                if (!isChecked[i][j] && grid[i][j] == '1') {
+                if (grid[i][j] == '1') {
                     dfs(grid, i, j);
                     count++;
                 }
@@ -18,10 +16,10 @@ public class Number_of_islands {
         return count;
     }
     private void dfs(char[][] grid, int i, int j) {
-        if (i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] == '0' || isChecked[i][j]) {
+        if (i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] == '0') {
             return;
         }
-        isChecked[i][j] = true;
+        grid[i][j] = '0';
         dfs(grid, i + 1, j);
         dfs(grid, i - 1, j);
         dfs(grid, i, j + 1);
