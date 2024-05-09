3b7425dceea0a5049913b8807395562193b5226e
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2024-05-09 12:22:02 +0200

    Maximize Happiness of Selected Children solution

---
+++ Daily_questions/Maximize_happiness_of_selected_children.java
@@ -0,0 +1,24 @@
+public class Maximize_happiness_of_selected_children {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        long ans = 0;
+        // Arrays.sort(happiness, (b, a) -> Integer.compare(a, b));
+        // Arrays.sort(happiness, Collections.reverseOrder());
+        Arrays.sort(happiness);
+        reverseArray(happiness);
+        for (int i = 0; i < k; i++) {
+            ans += happinessInTurn(happiness[i], i);
+        }
+        return ans;
+    }
+    private int happinessInTurn(int happiness, int turn) {
+        return (happiness - turn) >= 0 ? happiness - turn : 0;
+    }
+    private void reverseArray(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n / 2; i++) {
+            int temp = array[i];
+            array[i] = array[n - 1 - i];
+            array[n - 1 - i] = temp;
+        }
+    }
+}
