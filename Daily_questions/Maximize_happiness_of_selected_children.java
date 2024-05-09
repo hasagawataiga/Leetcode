04eca8e3c530afa2d7e88809c2a1f1b8fcb1ae6d
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2024-05-09 12:36:10 +0200

    Improve algorithm

--- Daily_questions/Maximize_happiness_of_selected_children.java
+++ Daily_questions/Maximize_happiness_of_selected_children.java
@@ -5,22 +5,17 @@ public class Maximize_happiness_of_selected_children {
         long ans = 0;
         // Arrays.sort(happiness, (b, a) -> Integer.compare(a, b));
         // Arrays.sort(happiness, Collections.reverseOrder());
+        int length = happiness.length;
         Arrays.sort(happiness);
-        reverseArray(happiness);
-        for (int i = 0; i < k; i++) {
-            ans += happinessInTurn(happiness[i], i);
+        int turns = 0;
+        for (int i = length - 1; i >= length - k; i--) {
+            int val = happiness[i] - turns++;
+            if (val > 0) {
+                ans += val;
+            } else {
+                break;
+            }
         }
         return ans;
-    }
-    private int happinessInTurn(int happiness, int turn) {
-        return (happiness - turn) >= 0 ? happiness - turn : 0;
-    }
-    private void reverseArray(int[] array) {
-        int n = array.length;
-        for (int i = 0; i < n / 2; i++) {
-            int temp = array[i];
-            array[i] = array[n - 1 - i];
-            array[n - 1 - i] = temp;
-        }
     }
 }
