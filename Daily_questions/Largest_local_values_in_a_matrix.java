48bc28bf1176c70b75b5bde80c50ae30b935b63a
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2024-05-12 12:54:28 +0200

    Largest Local Values in a Matrix solution

---
+++ Daily_questions/Largest_local_values_in_a_matrix.java
@@ -0,0 +1,27 @@
+public class Largest_local_values_in_a_matrix {
+    public int[][] largestLocal(int[][] grid) {
+        int col = grid[0].length;
+        int row = grid.length;
+        if(col < 3 || row < 3){
+            return new int[0][0];
+        }
+        int[][] result = new int[col-2][row-2];
+        for(int i = 0; i < result.length; i++){
+            for(int j = 0; j < result[0].length; j++){
+                result[i][j] = findMax(grid, i, j);
+            }
+        }
+        return result;
+    }
+    public int findMax (int[][] grid, int a, int b){
+        int max = Integer.MIN_VALUE;
+        for(int i = a; i < a+3; i++){
+            for(int j = b; j < b+3; j++){
+                if(grid[i][j] > max){
+                    max = grid[i][j];
+                }
+            }
+        }
+        return max;
+    }
+}
