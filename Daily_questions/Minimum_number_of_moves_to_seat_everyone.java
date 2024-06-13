34f41b3725c0e5e5c0749dcc05572cec48d8f634
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2024-06-13 17:09:22 +0200

    Add Comment to explain to algorithm

--- Daily_questions/Minimum_number_of_moves_to_seat_everyone.java
+++ Daily_questions/Minimum_number_of_moves_to_seat_everyone.java
@@ -3,6 +3,8 @@ public class Minimum_number_of_moves_to_seat_everyone {
     int[] order;
     public int minMovesToSeat(int[] seats, int[] students) {
         length = seats.length;
+        // each EmptySeat is stored as negative value (ex: 2 seats at position 2 are empty the value order[1] = -2)
+        // each NoSeatStudent is stored as positive value (ex: 2 no corrected seat students {3, 3}, the value order[3] = 2)
         order = new int[101];
         for (int i = 0; i < length; i++) {
             order[seats[i]]--;
@@ -11,6 +13,7 @@ public class Minimum_number_of_moves_to_seat_everyone {
         int emptySeat = nextEmptySeat(seats, 0);
         int student = nextStudent(students, 0);
         int count = 0;
+        // Iterate through order to get the gaps between no corrected seat student and the empty seats
         while (emptySeat < 101) {
             count += Math.abs(emptySeat - student);
             if (++order[emptySeat] == 0) {
