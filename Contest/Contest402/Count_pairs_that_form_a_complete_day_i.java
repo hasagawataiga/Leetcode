9b82357e083224a3d043b443f3f358e7e0332c9f
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2024-06-17 21:37:15 +0200

    Count Pairs that Form a Complete Day I solution

---
+++ Contest/Contest402/Count_pairs_that_form_a_complete_day_i.java
@@ -0,0 +1,15 @@
+package Contest.Contest402;
+
+public class Count_pairs_that_form_a_complete_day_i {
+    public int countCompleteDayPairs(int[] hours) {
+        int count = 0;
+        for (int i = 0; i < hours.length; i++) {
+            for (int j = i + 1; j < hours.length; j++) {
+                if ((hours[i] + hours[j]) % 24 == 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
