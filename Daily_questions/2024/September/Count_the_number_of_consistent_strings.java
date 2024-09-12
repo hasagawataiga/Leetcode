6bef3d0fdd9e835e391d1452f44f6c145fa13e25
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2024-09-12 09:27:05 +0200

    Adjust solution

--- Daily_questions/2024/September/Count_the_number_of_consistent_strings.java
+++ Daily_questions/2024/September/Count_the_number_of_consistent_strings.java
@@ -2,9 +2,13 @@ public class Count_the_number_of_consistent_strings {
     class Solution {
         public int countConsistentStrings(String allowed, String[] words) {
             int res = words.length;
+            boolean[] chars = new boolean[26];
+            for (char ch : allowed.toCharArray()) {
+                chars[ch - 'a'] = true;
+            }
             for (String word : words) {
                 for (char ch : word.toCharArray()) {
-                    if (!allowed.contains(ch + "")) {
+                    if (!chars[ch - 'a']) {
                         res--;
                         break;
                     }
