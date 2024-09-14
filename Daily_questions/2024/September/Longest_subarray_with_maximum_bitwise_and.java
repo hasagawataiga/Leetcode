fa6606a7da1e4cef92c9a140c300d9c3d951b5c5
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2024-09-14 23:17:52 +0200

    Longest Subarray with Maximum Bitwise AND solution

---
+++ Daily_questions/2024/September/Longest_subarray_with_maximum_bitwise_and.java
@@ -0,0 +1,20 @@
+public class Longest_subarray_with_maximum_bitwise_and {
+    class Solution {
+        public int longestSubarray(int[] nums) {
+            int maxVal = 0, ans = 0, currentStreak = 0;
+            for (int num : nums) {
+                if (maxVal < num) {
+                    maxVal = num;
+                    ans = currentStreak = 0;
+                }
+                if (maxVal == num) {
+                    currentStreak++;
+                } else {
+                    currentStreak = 0;
+                }
+                ans = Math.max(ans, currentStreak);
+            }
+            return ans;
+        }
+    }
+}
