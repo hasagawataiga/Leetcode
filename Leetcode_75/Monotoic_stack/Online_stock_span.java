724f4729c174fc95b2a40bf7d4ecaa1e10c6e9b6
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2024-09-24 20:44:04 +0200

    Monotoic Stack solution

---
+++ Leetcode_75/Monotoic_stack/Online_stock_span.java
@@ -0,0 +1,27 @@
+public class Online_stock_span {
+    class StockSpanner {
+        class Stock {
+            int val;
+            int span;
+            Stock(int val, int span) {
+                this.val = val;
+                this.span = span;
+            }
+            public int getVal() {return this.val;}
+            public int getSpan() {return this.span;}
+        }
+        Stack<Stock> stocks;
+        public StockSpanner() {
+            stocks = new Stack<>();       
+        }
+        
+        public int next(int price) {
+            int count = 1;
+            while (!stocks.isEmpty() && stocks.peek().getVal() <= price) {
+                count += stocks.pop().getSpan();
+            }
+            stocks.push(new Stock(price, count));
+            return count;
+        }
+    }
+}
