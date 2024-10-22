3b17250cf2ab197014caf57d10a2cb3ff780b410
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2024-10-22 13:45:27 +0200

    Import Library

--- Daily_questions/2024/October/Kth_largest_sum_in_a_binary_tree.java
+++ Daily_questions/2024/October/Kth_largest_sum_in_a_binary_tree.java
@@ -1,19 +1,22 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Kth_largest_sum_in_a_binary_tree {
-    /**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
+public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+
     class Solution {
         public long kthLargestLevelSum(TreeNode root, int k) {
             Map<Integer, Long> map = new HashMap<>();
