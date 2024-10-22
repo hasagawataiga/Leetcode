421b6c894877f020a18ef304d5e0f64ba6c7fb08
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2024-10-22 13:42:47 +0200

    Kth Largest Sum in a Binary Trees oltuion

---
+++ Daily_questions/2024/October/Kth_largest_sum_in_a_binary_tree.java
@@ -0,0 +1,43 @@
+public class Kth_largest_sum_in_a_binary_tree {
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+    class Solution {
+        public long kthLargestLevelSum(TreeNode root, int k) {
+            Map<Integer, Long> map = new HashMap<>();
+            traversal(root, map, 0);
+            int size = map.size();
+            if (size < k) {
+                return -1;
+            }
+            long[] levelSums = new long[size];
+            for (int i = 0; i < size; i++) {
+                levelSums[i] = map.get(i);
+            }
+            Arrays.sort(levelSums);
+            return levelSums[size - k];
+        }
+
+        private void traversal(TreeNode node, Map<Integer, Long> map, int level) {
+            if (node == null) {
+                return;
+            }
+            map.putIfAbsent(level, 0l);
+            map.put(level, map.get(level) + node.val);
+            traversal(node.left, map, level + 1);
+            traversal(node.right, map, level + 1);
+        }
+    }
+}
