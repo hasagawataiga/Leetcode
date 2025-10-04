1a4df9935b7d93be6ec46fcbc0e63a4e8f87a4e2
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2025-10-04 08:26:09 +0200

    Container with Most Water solution

---
+++ Daily_questions/Year_2025/October/Container_with_most_water.java
@@ -0,0 +1,23 @@
+package Daily_questions.Year_2025.October;
+
+public class Container_with_most_water {
+    class Solution {
+        public int maxArea(int[] height) {
+            int n = height.length;
+            int left = 0;
+            int right = n - 1;
+            int preArea = 0;
+            int max = 0;
+            while (left < right){
+                int area = (int) ((right - left) * Math.min(height[right], height[left]));
+                max = Math.max(max, area);
+                if (height[right] > height[left]){
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+            return max;
+        }
+    }
+}
